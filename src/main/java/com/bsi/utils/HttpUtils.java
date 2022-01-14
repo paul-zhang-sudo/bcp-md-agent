e33c4ea2fcb79c1820ff501cdb463c7b5b6d1660
fish <java_fish@126.com>
2022-01-14 11:49:38 +0800

    修改线程池

--- src/main/java/com/bsi/utils/HttpUtils.java
+++ src/main/java/com/bsi/utils/HttpUtils.java
@@ -44,21 +44,7 @@ public class HttpUtils {
      * @return AgHttpResult
      */
     public static AgHttpResult request(String method, String url, Map<String,String> headers, String body){
-        //插件式配置生成HttpClient时所需参数（超时、连接池、ssl、重试）
-        HttpClient client = null;
-        try{
-            HCB hcb = HCB.custom()
-                    .sslpv(SSLs.SSLProtocolVersion.TLSv1_2) 	//可设置ssl版本号，默认SSLv3，用于ssl，也可以调用sslpv("TLSv1.2")
-                    .ssl()  			   		//https，支持自定义ssl证书路径和密码，ssl(String keyStorePath, String keyStorepass)
-                    .pool(500, 50)    	//启用连接池，每个路由最大创建50个链接，总连接数限制为500个
-                    .retry(2);					//重试2次
-            client = hcb.build();
-        }catch (HttpProcessException e){
-            log.info("创建httpclient报错,报错信息:",ExceptionUtils.getFullStackTrace(e));
-        }
-
-        HttpConfig config = HttpConfig.simpleCustom(80000).client(client);
-//        HttpConfig config = HttpConfig.simpleCustom(80000);
+        HttpConfig config = HttpConfig.simpleCustom(80000);
         HttpHeader header = HttpHeader.custom();
         if( MapUtils.isNotEmpty(headers) ){
             for( String key:headers.keySet() ){
