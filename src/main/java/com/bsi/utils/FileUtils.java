2c080fc734fbab893e877cb136b79222664916ba
zhangsiqiang <zhangsiqiang@bizsoftinfo.com>
2022-02-25 10:53:56 +0800

    文件工具类增加UTF-8编码

--- src/main/java/com/bsi/utils/FileUtils.java
+++ src/main/java/com/bsi/utils/FileUtils.java
@@ -4,6 +4,7 @@ import com.bsi.framework.core.utils.ExceptionUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
@@ -20,7 +21,7 @@ public class FileUtils {
     public static List<String> readFile(String path){
         List<String> resultLines = null;
         try{
-            resultLines = org.apache.commons.io.FileUtils.readLines(new File(path),"utf-8");
+            resultLines = org.apache.commons.io.FileUtils.readLines(new File(path),StandardCharsets.UTF_8);
         }catch (Exception e){
             log.error("读取文件{}报错,错误信息:", ExceptionUtils.getFullStackTrace(e));
         }
@@ -36,7 +37,7 @@ public class FileUtils {
         boolean flag = true;
         List<String> resultLines = null;
         try{
-            org.apache.commons.io.FileUtils.write(new File(path),msg,append);
+            org.apache.commons.io.FileUtils.write(new File(path),msg, StandardCharsets.UTF_8,append);
         }catch (Exception e){
             flag = false;
             log.error("写入文件{}报错,错误信息:", ExceptionUtils.getFullStackTrace(e));
