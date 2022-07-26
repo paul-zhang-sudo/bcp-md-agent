ebd172e71cddc530cd90d46c5679248daea31c7d
fish <java_fish@126.com>
2022-07-26 11:13:22 +0800

    修改ot数据下发配置

--- src/main/java/com/bsi/md/agent/ot/AgDcDriver.java
+++ src/main/java/com/bsi/md/agent/ot/AgDcDriver.java
@@ -1,13 +1,18 @@
 package com.bsi.md.agent.ot;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.bsi.md.agent.entity.dto.AgConfigDto;
+import com.bsi.md.agent.entity.dto.AgDataSourceDto;
+import com.bsi.md.agent.service.AgConfigService;
+import com.bsi.md.agent.service.AgDataSourceService;
 import com.huaweicloud.sdk.iot.module.DcClient;
 import com.huaweicloud.sdk.iot.module.ModuleShadowNotificationCallback;
 import com.huaweicloud.sdk.iot.module.PointsCallback;
 import com.huaweicloud.sdk.iot.module.dto.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 
 @Slf4j
@@ -19,6 +24,8 @@ public class AgDcDriver implements PointsCallback, ModuleShadowNotificationCallb
      */
     private DcClient dcClient;
 
+    private AgConfigService agConfigService;
+    private AgDataSourceService agDataSourceService;
     @PostConstruct
     void init() throws Exception {
         log.info("初始化dcClient");
@@ -41,6 +48,74 @@ public class AgDcDriver implements PointsCallback, ModuleShadowNotificationCallb
     public void onModuleShadowReceived(ModuleShadowNotification shadow) {
         log.info("收到ot下发的配置信息。。。");
         log.info("shadow:{}", JSON.toJSONString(shadow));
+
+        JSONObject obj = JSON.parseObject( JSON.toJSONString(shadow) );
+        JSONObject defaultValue = obj.getJSONObject("properties").getJSONObject("default_values");
+        //初始化数据源
+        JSONObject otParam = obj.getJSONObject("connection_info");
+        JSONArray dsArr = defaultValue.getJSONArray("bcp_ds");
+        for(int i=0;i<dsArr.size();i++){
+            AgDataSourceDto dto = JSON.parseObject(dsArr.getString(i),AgDataSourceDto.class);
+            JSONObject cfv = JSON.parseObject(dto.getConfigValue());
+            if(cfv.size()>0){
+                log.info("进行属性替换");
+                cfv.forEach((k,v)->{
+                    cfv.put(k,otParam.getOrDefault(dto.getId()+"_"+k,v));
+                });
+                dto.setConfigValue(cfv.toJSONString());
+            }
+            log.info("要更新的数据源:{}",JSON.toJSONString(dto));
+            //刷新数据源
+            agDataSourceService.updateDS(dto);
+        }
+        //初始化配置
+        AgConfigDto cfn = JSON.parseObject(defaultValue.getString("bcp_conf"),AgConfigDto.class);
+        //配置中有些配置参数需要从点位中取值 TODO
+        if(cfn!=null){
+            agConfigService.updateConfig( cfn );
+        }
+
+        //根据前缀把数据源拆分出来
+//        Map<String,JSONObject> dsMap = new HashMap<>();
+//        ds.forEach((k,v)->{
+//            if(k.indexOf("script")>=0){
+//                return;
+//            }
+//            String key = k.substring(0,k.indexOf("_"));
+//            JSONObject tmp = dsMap.get(key);
+//            if(tmp==null){
+//                tmp = new JSONObject();
+//                dsMap.put(k,tmp);
+//            }
+//            tmp.put(k.replace(key,""),v);
+//        });
+//        String[] excludeProperties = {"id", "name","type","classify","delFlag"};
+//        PropertyPreFilters filters = new PropertyPreFilters();
+//        PropertyPreFilters.MySimplePropertyPreFilter excludeFilter = filters.addFilter();
+//        excludeFilter.addExcludes(excludeProperties);
+        //组装、刷新数据源
+//        dsMap.values().forEach(a->{
+//            AgDataSourceDto dto = new AgDataSourceDto();
+//            dto.setId(a.getString("id"));
+//            dto.setName(a.getString("name"));
+//            dto.setType(a.getString("type"));
+//            dto.setClassify( a.getString("classify") );
+//            dto.setDelFlag(a.getBoolean("delFlag")==null?false:a.getBoolean("delFlag"));
+//            dto.setConfigValue(JSON.toJSONString(obj,excludeFilter));
+//            //刷新数据源
+//            agDataSourceService.updateDS(dto);
+//        });
+
+        //组装、刷新config
+//        JSONObject conf = obj.getJSONObject("points");
+//        conf.forEach((k,v)->{
+//            JSONObject val = (JSONObject) v;
+//            AgConfigDto dto = new AgConfigDto();
+//            dto.setId(val.getString("id"));
+//            dto.setName(val.getString("name"));
+//            dto.setConfigValue(val.getString("configValue"));
+//            agConfigService.updateConfig(dto);
+//        });
     }
 
     /**
@@ -57,5 +132,10 @@ public class AgDcDriver implements PointsCallback, ModuleShadowNotificationCallb
     @Override
     public PointsGetRsp onPointGet(String requestId, PointsGetReq pointsGetReq) {
         return null;
+    }
+
+    private String generateCron(int minute){
+        String cron = "0 %s * * * ?";
+        return String.format(cron,minute<60?("0/"+minute):"0/59");
     }
 }
