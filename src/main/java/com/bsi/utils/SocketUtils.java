5a1c7b926bb3f1a8127064ac62c5d75981233a84
fish <java_fish@126.com>
2023-11-02 17:05:59 +0800

    日志加日志

--- src/main/java/com/bsi/utils/SocketUtils.java
+++ src/main/java/com/bsi/utils/SocketUtils.java
@@ -1,6 +1,7 @@
 package com.bsi.utils;
 
 import com.bsi.framework.core.utils.ExceptionUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,12 +10,17 @@ import java.util.HashMap;
 /**
  * socket工具类
  */
+@Slf4j
 public class SocketUtils {
     private static Logger info_log = LoggerFactory.getLogger("TASK_INFO_LOG");
     private static HashMap<String,SocketClient> clientMap = new HashMap<>();
 
     public static SocketClient getClient(String key,String ip,int port){
+        info_log.info("key:{},ip:{},port:{}",key,ip,port);
+        log.info("key:{},ip:{},port:{}",key,ip,port);
         SocketClient client = clientMap.get(key);
+        info_log.info("client:{}",client);
+        log.info("client:{}",client);
         if(client==null){
             info_log.info("client对象不存在,创建新的client对象");
             client = new SocketClient();
