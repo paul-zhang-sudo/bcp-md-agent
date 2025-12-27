bd39404e4a40168b49f32e1d6e726d6710c0a49f
fish <java_fish@126.com>
2025-12-27 10:04:11 +0800

    update

---
+++ src/main/java/com/bsi/utils/DcClientUtils.java
@@ -0,0 +1,41 @@
+package com.bsi.utils;
+
+import com.bsi.framework.core.utils.FwSpringContextUtil;
+import com.bsi.md.agent.ot.AgDcDriver;
+import com.huawei.m2m.edge.daemon.util.JacksonUtil;
+import com.huaweicloud.sdk.iot.module.DcClient;
+import com.huaweicloud.sdk.iot.module.dto.PointsReport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * OT数采工具类
+ */
+public class DcClientUtils {
+
+    private static Logger info_log = LoggerFactory.getLogger("TASK_INFO_LOG");
+
+    private DcClient getDcClient(){
+        return FwSpringContextUtil.getBean("agDcDriver",AgDcDriver.class).getDcClient();
+    }
+
+    /**
+     *
+     * @param points
+     * @return
+     */
+    public boolean reportPoints(Map<String, Object> points) {
+        PointsReport report = new PointsReport();
+        report.setPoints(points);
+        try {
+            info_log.info("需要上报的数据:{}", JacksonUtil.pojo2Json(report));
+            getDcClient().pointReport(report);
+            return true;
+        } catch (Exception e) {
+            info_log.error("数据上报出现错误 ",e);
+            return false;
+        }
+    }
+}
